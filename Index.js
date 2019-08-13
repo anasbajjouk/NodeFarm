1fffc1405b8e8196283cf44c58cb9fd3434ac596
anasbajjouk <34046200+anasbajjouk@users.noreply.github.com>
2019-08-13 17:18:49 +0200

    Updated the app by adding a customised module

--- Index.js
+++ Index.js
@@ -1,22 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 
-// Replace Tempalte fucntion
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-    return output;
-}
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // Reading templates
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
@@ -26,6 +13,9 @@ const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product
 // Reading data from our data.json file
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
+
+const slugs = dataObject.map(element => slugify(element.productName, {lower:true}));
+console.log(slugs);
 
 // Create server
 const server = http.createServer((request, response) => {
